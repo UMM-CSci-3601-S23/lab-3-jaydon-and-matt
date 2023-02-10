ee343440b05cc508557f7e16b91111eb2ca4b9c8
Jay <stani152@morris.umn.edu>
2023-02-09 18:45:16 -0600

    create todo service and todo class
    we will use the todo service to implement some functionality for todo-list-component and possibly other things if we need to
    Co-authored-by: Matthew Wanner <wanne051@morris.umn.edu>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -30,7 +30,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListComponent } from './todos/todo-list.component';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
