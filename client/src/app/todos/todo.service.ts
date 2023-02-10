70d3e795da4eb5b50cbc31a62549cb71c3dc007e
Jay <stani152@morris.umn.edu>
2023-02-10 12:50:19 -0600

    display todos on the client
    the todos show up now, but they're kind of ugly, so we're going to change some stuff to make it more "presentable"
    Co-authored-by: Matthew Wanner <wanne051@morris.umn.edu>

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -29,4 +29,33 @@ export class TodoService {
   getTodoById(id: string): Observable<Todo> {
     return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
   }
+
+  /**
+   * A service method that filters an array of `Todo` using
+   * the specified filters.
+   *
+   * Note that the filters here support partial matches. Since the
+   * matching is done locally we can afford to repeatedly look for
+   * partial matches instead of waiting until we have a full string
+   * to match against.
+   *
+   * @param todos the array of `Todos` that we're filtering
+   * @param filters the map of key-value pairs used for the filtering
+   * @returns an array of `Todos` matching the given filters
+   */
+  filterTodos(todos: Todo[]): Todo[] {
+    const filteredTodos = todos;
+    // TODO: allow filtering on the client side (use the methods below as a template). filteredTodos won't be a constant
+    /*if (filters.name) {
+      filters.name = filters.name.toLowerCase();
+      filteredTodos = filteredTodos.filter(user => user.name.toLowerCase().indexOf(filters.name) !== -1);
+    }
+
+    if (filters.company) {
+      filters.company = filters.company.toLowerCase();
+      filteredTodos = filteredTodos.filter(user => user.company.toLowerCase().indexOf(filters.company) !== -1);
+    }*/
+
+    return filteredTodos;
+  }
 }
