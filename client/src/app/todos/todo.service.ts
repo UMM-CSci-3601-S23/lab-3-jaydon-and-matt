7861470b611359954b68320ba0b15b024fb1fdc2
Jay <stani152@morris.umn.edu>
2023-02-10 12:39:38 -0600

    add functionality to todo service to fetch todos from server
    the service will not filter any todos yet, this will come later
    Co-authored-by: Matthew Wanner <wanne051@morris.umn.edu>

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Todo } from './todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  // The URL for the users part of the server API.
+  readonly todoUrl: string = environment.apiUrl + 'todos'; // /api/todos , sent to the Server
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getTodos(): Observable<Todo[]> {
+    // TODO: support filtering the todos by its properties... we will do this with an httpParams map
+
+    // `HttpParams` is essentially just a map used to hold key-value
+    // pairs that are then encoded as "?key1=value1&key2=value2&…" in
+    // the URL when we make the call to `.get()` below.
+
+    // Send the HTTP GET request with the given URL and parameters.
+    // That will return the desired `Observable<User[]>`.
+    return this.httpClient.get<Todo[]>(this.todoUrl, { // /api/users/{parameters here}
+    });
+  }
+
+  getTodoById(id: string): Observable<Todo> {
+    return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
+  }
 }
