4049eaf012c5755f763235eef00e19af83b177ff
Jay <stani152@morris.umn.edu>
2023-02-15 17:54:40 -0600

    set up tests for the todo service and component functionality
    we're still kind of iffy on how testing works with karma, and what exactly we're supposed to be testing with all of this...
    Co-authored-by: Matthew Wanner <wanne051@morris.umn.edu>

--- client/src/app/todos/todo.service.spec.ts
+++ client/src/app/todos/todo.service.spec.ts
@@ -1,16 +1,43 @@
-import { TestBed } from '@angular/core/testing';
-
 import { TodoService } from './todo.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { Todo } from './todo';
+
 
 describe('TodoService', () => {
-  let service: TodoService;
+
+  const testUsers: Todo[] = [
+    {
+      _id: 'chris_id',
+      owner: 'Chris',
+      status: true,
+      body: 'test',
+      category: 'video games'
+    },
+  ];
+  let todoService: TodoService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoService);
+    // Set up the mock handling of the HTTP requests
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    // Construct an instance of the service with the mock
+    // HTTP client.
+    todoService = new TodoService(httpClient);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(todoService).toBeTruthy();
   });
 });
---
+++ client/src/testing/todo.service.mock.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Todo } from '../app/todos/todo';
+import { TodoService } from '../app/todos/todo.service';
+
+/**
+ * A "mock" version of the `TodoService` that can be used to test components
+ * without having to create an actual service. It needs to be `Injectable` since
+ * that's how services are typically provided to components.
+ */
+@Injectable()
+export class MockTodoService extends TodoService {
+  static testTodos: Todo[] = [
+    {
+      _id: 'chris_id',
+      owner: 'Chris',
+      status: true,
+      body: 'test',
+      category: 'video games'
+    },
+  ];
+
+  constructor() {
+    super(null);
+  }
+
+  getTodos(): Observable<Todo[]> {
+    // Our goal here isn't to test (and thus rewrite) the service, so we'll
+    // keep it simple and just return the test users regardless of what
+    // filters are passed in.
+    //
+    // The `of()` function converts a regular object or value into an
+    // `Observable` of that object or value.
+    return of(MockTodoService.testTodos);
+  }
+
+  getTodoById(id: string): Observable<Todo> {
+    // If the specified ID is for the first test todo,
+    // return that todo, otherwise return `null` so
+    // we can test illegal todo requests.
+    if (id === MockTodoService.testTodos[0]._id) {
+      return of(MockTodoService.testTodos[0]);
+    } else {
+      return of(null);
+    }
+  }
+
+}
