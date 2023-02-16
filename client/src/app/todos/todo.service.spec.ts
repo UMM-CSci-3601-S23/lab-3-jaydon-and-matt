668711c1ee0f5be68dc1a3c18c8ad675435f42f5
Jay <stani152@morris.umn.edu>
2023-02-15 18:15:50 -0600

    implement testing for the todo service
    right now it won't test for filtering anything, since we don't actually have capabilities for filtering yet... that'll be up next

--- client/src/app/todos/todo.service.spec.ts
+++ client/src/app/todos/todo.service.spec.ts
@@ -7,7 +7,7 @@ import { Todo } from './todo';
 
 describe('TodoService', () => {
 
-  const testUsers: Todo[] = [
+  const testTodos: Todo[] = [
     {
       _id: 'chris_id',
       owner: 'Chris',
@@ -39,5 +39,54 @@ describe('TodoService', () => {
 
   it('should be created', () => {
     expect(todoService).toBeTruthy();
+  });
+
+  it('calls `api/todos` when `getTodos()` is called with no parameters', () => {
+    // Assert that the users we get from this call to getTodos()
+    // should be our set of test todos. Because we're subscribing
+    // to the result of getTodos(), this won't actually get
+    // checked until the mocked HTTP request 'returns' a response.
+    // This happens when we call req.flush(testTodos) a few lines
+    // down.
+    todoService.getTodos().subscribe(
+      todos => expect(todos).toBe(testTodos)
+    );
+
+    // Specify that (exactly) one request will be made to the specified URL.
+    const req = httpTestingController.expectOne(todoService.todoUrl);
+    // Check that the request made to that URL was a GET request.
+    expect(req.request.method).toEqual('GET');
+    // Check that the request had no query parameters.
+    expect(req.request.params.keys().length).toBe(0);
+    // Specify the content of the response to that request. This
+    // triggers the subscribe above, which leads to that check
+    // actually being performed.
+    req.flush(testTodos);
+  });
+
+  describe('getTodosByID()', () => {
+    it('calls api/todos/id with the correct ID', () => {
+      // We're just picking a Todo "at random" from our little
+      // set of Todos up at the top.
+      const targetTodo: Todo = testTodos[0];
+      const targetId: string = targetTodo._id;
+
+      todoService.getTodoById(targetId).subscribe(
+        // This `expect` doesn't do a _whole_ lot.
+        // Since the `targetTodo`
+        // is what the mock `HttpClient` returns in the
+        // `req.flush(targetTodo)` line below, this
+        // really just confirms that `getTodoByid()`
+        // doesn't in some way modify the user it
+        // gets back from the server.
+        todo => expect(todo).toBe(targetTodo)
+      );
+
+      const expectedUrl: string = todoService.todoUrl + '/' + targetId;
+      const req = httpTestingController.expectOne(expectedUrl);
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(targetTodo);
+    });
   });
 });
